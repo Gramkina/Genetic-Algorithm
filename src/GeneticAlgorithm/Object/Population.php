581c315af779da2eaf955a925dbc0658db099a50
Andrej <s>
2021-12-25 05:19:15 +0400

    Change generation population

--- src/GeneticAlgorithm/Object/Population.php
+++ src/GeneticAlgorithm/Object/Population.php
@@ -5,16 +5,51 @@ namespace Gramk\GeneticAlgorithm\Object;
 class Population
 {
     /**
-     * @var array Individual
+     * Individuals
+     *
+     * @var Individual[]
      */
     protected $individuals = [];
 
-    public function __construct($countIndividual, $countChromosomes)
+    /**
+     * Count of individual
+     *
+     * @var int
+     */
+    protected $countIndividual;
+
+    /**
+     * Count individual chromosomes
+     *
+     * @var int
+     */
+    protected $countChromosomes;
+
+    /**
+     * @param $countIndividual int
+     * @param $countChromosomes int
+     * @param $autoGenerate bool
+     */
+    public function __construct($countIndividual, $countChromosomes, $autoGenerate=null)
     {
-        for ($i = 0; $i < $countIndividual; $i++) {
-            $individual = new Individual($countChromosomes);
-            array_push($this->individuals, $individual);
+        $this->countIndividual = $countIndividual;
+        if ($autoGenerate) {
+            for ($i = 0; $i < $countIndividual; $i++) {
+                $individual = new Individual($countChromosomes, true);
+                $this->individuals[] = $individual;
+            }
         }
+    }
+
+    /**
+     * Set individuals in population
+     *
+     * @param $arrayIndividuals Individual[]
+     * @return bool
+     */
+    public function setIndividuals($arrayIndividuals) {
+        $this->individuals = $arrayIndividuals;
+        return true;
     }
 
     /**
@@ -29,6 +64,7 @@ class Population
 
     /**
      * Sort individuals by fitness
+     *
      * @return void
      */
     public function sortByFitness()
